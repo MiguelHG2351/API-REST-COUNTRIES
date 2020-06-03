7bb0f96b36abbb4b2c364a94a9c70c05bc50bc02
Miguel Hernández Gaitan <60478285+MiguelHG2351@users.noreply.github.com>
2020-06-02 19:40:28 -0600

    Listo 1

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,8 @@ module.exports = {
               {
                 loader: 'file-loader',
               },
+            ]
+          },
           {
               test: /\.ejs$/,
               use: [
